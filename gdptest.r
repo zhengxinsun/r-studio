ab537c68b55bd73dc240da2ff72d19370e2d78f4
Zhengxin Sun <sunz@queensu.ca>
2017-04-25 15:36:03 -0400

    add ggplot and dplyr package

--- gdptest.r
+++ gdptest.r
@@ -46,7 +46,7 @@ wrong <- gap [gap$county == c("China", "Canada"),]
 
 wrong
 
-# right uses %L% ?
+# right uses %>% ?
 
 # write function  from Ferrenheit to Calvin
 
@@ -55,3 +55,73 @@ Ftocalvin <- function (ftemp) (
 Ftocalvin (12)
 
 # free
+
+library (dplyr)
+
+select (gapminder, continent)
+select (gapminder, matches("cont"))
+
+filter (gapminder, continent =="Africa")
+
+mutate (gapminder, meanlife = mean (lifeExp))
+
+transmute ()
+
+smmerize ()
+
+gapminder %>%
+  filter (continent == "Africa") %>%
+  group_by(country) %>%
+  summarize (min_life = min (lifeExp),
+             max_life = max (lifeExp))%>% View
+
+ gapminder %>%
+   filter (continent =="Africa") %>%
+   select (lifeExp, country, year)%>% View
+
+ gapminder %>%
+   group_by(country) %>%
+   mutate (meanlife = mean (lifeExp)) %>% select (meanlife)%>%
+    View
+ 
+ summarize_each ()
+ summarize_all ()
+ summarize_at ()
+ summarize_if()
+ 
+ library (tidyr)
+ gather ()
+ 
+ gather (gapminder, "obs_var", "obs_key", lifeExp, gdpPercap) %>%
+   group_by(obs_var) %>% summarize (means =mean (obs_key))
+ 
+ spread ()
+ 
+ gaplong <- gather (gapminder, "obs_var", "obs_val", lifeExp, gdpPercap, pop)
+   
+ str (gaplong)
+
+ gaplong %>% group_by(continent,obs_var) %>%
+   summarize(means=mean(obs_val))
+ 
+ spread (gaplong, year, obs_val) %>% View
+ 
+ gap_united <- unite (gapminder, "continent_year", continent, year, sep = "+")
+ 
+ gap_united %>% 
+   separate (continent_year, c("continent", "year"), sep = "\\+")
+ 
+library (ggplot2)
+ 
+ ggplot (gapminder, aes(x = year, y = lifeExp, group = country)) + 
+   geom_point (aes(color = continent))
+ 
+ ggplot (gapminder, aes(x = year, y = lifeExp, group = country)) + 
+   geom_line (aes(color = continent)) +
+   scale_y_continuous(limits = c (20, 100))
+ 
+head (gapminder)
+ 
+ggplot (data = gapminder, aes (x =year, y = gdpPercap, group =country)) +
+  geom_line (aes(color = continent))
+ 
